c14eb857bbec36d7bd365ae5b50a57cab8c0538d
Maahnoor <maahnoork22@gmail.com>
2022-03-01 12:28:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+if (window.matchMedia('(max-width: 767px)').matches){
+$('.dropdown-content').hide();
+}
+$(document).ready(function(){
+
+  // jQuery methods go here...
+
+$('.dropbtn').click(function(){
+
+$('.dropdown-content').slideToggle(800,function(){
+	if ($('.dropbtn').attr('src')=='images/icon-hamburger.svg')
+  		$(".dropbtn").attr("src","images/icon-close.svg");
+	else if ($('.dropbtn').attr('src')=='images/icon-close.svg')
+ 		$(".dropbtn").attr("src","images/icon-hamburger.svg");
+  	
+
+});
+})
+
+$(window).resize(function(){
+            if($(window).width() > 768){
+            	$('.dropdown-content').removeAttr('style');
+            }
+            });
+
+});
